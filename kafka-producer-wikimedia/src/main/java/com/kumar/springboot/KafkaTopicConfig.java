cbb93446557313158c708ba706a832be3bb43894
DK2698 <dileepkumar1823@gmail.com>
2023-03-13 02:42:03 +0530

    fetch realtime data from wikimedia

--- kafka-producer-wikimedia/src/main/java/com/kumar/springboot/KafkaTopicConfig.java
+++ kafka-producer-wikimedia/src/main/java/com/kumar/springboot/KafkaTopicConfig.java
@@ -1,24 +1,20 @@
-package com.kumar.springbootkafka.config;
+package com.kumar.springboot;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.internals.Topic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+
     @Bean
     public NewTopic demoTopic(){
-        return TopicBuilder.name("demo")
-                .partitions(10)
+        return TopicBuilder.name("wikimedia-recent-change")
+                .partitions(20)
                 .build();
     }
 
-
-    @Bean
-    public NewTopic jsonTopic(){
-        return TopicBuilder.name("json-demo")
-                .build();
-    }
 
 }
